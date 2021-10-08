1b15ffec1fff77128562c5a77873305e447683c1
Meri <mgogi13@freeuni.edu.ge>
2021-10-08 14:26:34 +0400

    Bugged

---
+++ bugging.js
@@ -0,0 +1,106 @@
+const barMenu = document.querySelector('.fa-bars');
+const mobileMenu = document.getElementById('menu');
+const closedMenu = document.querySelector('.fa-times');
+const menuLists = document.querySelectorAll('.mobile-menu');
+const popup = document.querySelector('.work-popup');
+const mainBody = document.querySelector('body');
+const openPopup = document.querySelectorAll('.btn');
+const overlay = document.querySelector('.wrapper');
+
+function show() {
+  barMenu.style.display = 'none';
+  mobileMenu.classList.add('active');
+}
+
+function close() {
+  mobileMenu.classList.remove('active');
+  barMenu.style.display = 'block';
+}
+
+barMenu.addEventListener('click', show);
+closedMenu.addEventListener('click', close);
+for (let i = 0, i < menuLists.length; i += 1) {
+  menuLists[i].addEventListener('click', close);
+}
+
+const projects = {
+  name: 'multi-post stories',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
+  img: 'images/popup.png',
+  language1: 'css',
+  language2: 'html',
+  language3: 'bootstrap',
+  language4: 'Ruby',
+  liveLink: 'See Live',
+  sourceLink: 'See Source',
+};
+window.addEventListener('DOMContentLoaded', () => {
+  popup.innerHTML = `<div class="popup-div">
+    <div class="header-popup">
+      <h2 class="title-popup">${projects.name}</h2>
+      <img src="./images/times.png" class="whatever" alt="close-icon">
+    </div>
+    <div class="project-popup">
+      <img src="${projects.img}" alt="image-popup" class="image-popup">
+      <div class="info-popup">
+        <p>${projects.description}</p>
+        <ul class="">
+          <li class="list-popup" id="left-border">${projects.language1}</li>
+          <li class="list-popup">${projects.language2}</li>
+          <li class="list-popup">${projects.language3}</li>
+          <li class="list-popup">${projects.language4}</li>
+        </ul>
+        <div class="popup-buttons">
+          <button  class="btn1">
+          <a href="https://meri-mg.github.io/Portfolio-Microverse/">${projects.liveLink}</a>
+          <img src="./images/icon-live.png" alt="live-icon">
+          </button>
+          <button class="btn1">
+              <a href="https://github.com/Meri-MG/Portfolio-Microverse">${projects.sourceLink}</a>
+              <img src="./images/vector.png" alt="source-icon">
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  const closedPopup = document.querySelector('.whatever');
+  closedPopup.addEventListener('click', () => {
+    mainBody.classList.remove('fixed');
+    popup.classList.remove('open');
+  });
+});
+
+window.addEventListener('scroll', () => {
+  const scroll = this.pageYOffset;
+  if (scroll > 50) {
+    overlay.classList.remove('active');
+  }
+});
+
+openPopup.forEach((e) => {
+  e.addEventListener('click', () => {
+    popup.classList.add('open');
+    mainBody.classList.add('fixed');
+  });
+});
+
+const id = (id) => document.getElementById(id);
+const classes = (classes) => document.getElementsByClassName(classes);
+const email = id('email');
+const form = id('form');
+const errorMsg = classes('error');
+
+const validation = (id, serial, message) => {
+  if (id.value !== id.value.toLowerCase()) {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = '2px solid red';
+  } else {
+    errorMsg.innerHTML = '';
+    form.submit();
+  }
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validation(email, 1, 'Email should be written in lowercase');
+});
